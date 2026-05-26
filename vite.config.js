f0ef733f386820b7348fdcec3ae26bde8cd98f5a
juan6park <juan6park@gmail.com>
2026-05-26 15:32:55 +0900

    url 수정

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), basicSsl()],
+  plugins: [react(), tailwindcss()],
   server: {
-    https: true,
+    host: '127.0.0.1',
     port: 5173,
+    strictPort: true,
   },
 })
